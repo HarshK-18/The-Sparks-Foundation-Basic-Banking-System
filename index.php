751a4a023f68370bbfa145024d9f91d754f33ef1
HarshK-18 <99188277+HarshK-18@users.noreply.github.com>
2023-01-20 23:03:33 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <!-- font awesome -->
+  <script src="https://kit.fontawesome.com/d2747108de.js" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+    crossorigin="anonymous"></script>
+  <title>Home</title>
+  <style>body {
+    background-image: url("Homepage-1.jpg");
+    background-size: cover  ;
+    height: 100%;
+    width: 100% ;
+    
+    background-repeat:  no-repeat;
+    background-attachment:  fixed;
+    }</style>
+</head>
+
+<body>
+  <!-- _______NAVIGATION BAR_______ -->
+  <div id="title">
+    <nav id="navbar" class="navbar navbar-expand-lg navbar-dark ">
+      <a id="navbar-brand" href="index.php">All Citizens<span>  Bank</span></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01" aria-expanded"false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#footer">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About  </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+</div>
+
+  <div class="intro">
+    Welcome to our Banking Service !!
+  </div>
+  <div class="cont">
+    <form action="">
+      <br>
+      <button id="vcustomer" class="btn btn-primary btn-lg btn-block " name="View Customer" value="View Customer"
+        formaction="customer.php">Transfer Money</button><br>
+      <button id="vhistory" class="btn btn-primary btn-lg btn-block " name="Transaction History"
+        value="Transaction History" formaction="history.php">Transaction History</button>
+    </form>
+  </div>
+</body>
+<footer id="footer">
+  <a href="#"></a><i class="social-icon fas fa-phone-square-alt" ></i>
+  <a href="https://www.linkedin.com/in/harsh-khodake-b7b86420a/"><i class="social-icon fab fa-linkedin-in"></i></a>
+  <a href="#"><i class="social-icon fas fa-envelope-square"></i>
+  <p>©️ Copyright 2023 All Citizens Bank</p>
+</footer>
+
+</html>
